ee1732c9edcab9a138714c1f1d97fca369bbfb1f
user <user@DESKTOP-I6SPLCP>
2017-01-22 00:27:32 +0400

    Solar System using Indexers and Exception

---
+++ Solar System/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solar_System
+{
+    class SolarSystem
+    {
+        string[] planets = { "The Sun", "Mercury", "Venus", "Earth",
+            "Mars", "Jupiter", "Saturn","Uranus", "Neptune", "Pluto" };
+        private int GetPlanet(string testPlanet)
+        {
+            for (int j = 0; j < planets.Length; j++)
+            {
+                if (planets[j] == testPlanet)
+                {
+                    return j;
+                }
+            }
+            throw new ArgumentOutOfRangeException
+                (testPlanet, "testPlanet must be correct name of planet");
+        }
+        public int this[string planet]
+        {
+            get
+            {
+                return GetPlanet(planet);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolarSystem plan = new SolarSystem();
+            Console.WriteLine("Please enter name of planet to know its number from the - The Sun, Mercury, Venus, Earth,Mars, Jupiter, Saturn, Uranus, Neptune, Pluto ");
+            string st = Console.ReadLine();
+            Console.WriteLine(plan[st]);
+
+            //It's for Exception
+            //Console.WriteLine(plan["Marsss"]);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
